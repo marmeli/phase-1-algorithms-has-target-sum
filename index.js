4b6470df0f780c0700a3c25b9b78b0016526e30e
melissamariano <melissams538@gmail.com>
2023-05-28 23:51:00 -0400

    target sum

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
-}
+  for (let i = 0; i < array.length - 1; i++) {
+    for(let m = i + 1; m < array.length; m++) {
+      if (array[i] + array[m] === target) {
+        return true;
+      }
+    }
+  }
+  return false;
+  }
 
 /* 
   Write the Big O time complexity of your function here
